a0e37416dda39ec3df4bd655e079a047ca334737
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-12-01 23:32:32 -0300

    feat: Initialize the basic Vue app structure with routing and home page setup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
+import HomeView from "@/pages/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home-view",
+            component: HomeView
+        }
+    ],
 })
 
 export default router
