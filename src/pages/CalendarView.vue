79340a57a2f006b7ee886d7624356238eb293c04
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-12-17 00:28:56 -0300

    refactor: Remove AppHeader, AppFooter, and layout boilerplate from GraphView and CalendarView pages.

--- src/pages/CalendarView.vue
+++ src/pages/CalendarView.vue
@@ -1,15 +1,7 @@
 <template>
-    <div class="min-h-screen flex flex-col bg-white dark:bg-gray-950">
-        <AppHeader />
-        <main class="grow container mx-auto px-4 py-8">
-            <StudyCalendar />
-        </main>
-        <AppFooter />
-    </div>
+    <StudyCalendar />
 </template>
 
 <script setup lang="ts">
-import AppHeader from '@/components/AppHeader.vue';
-import AppFooter from '@/components/AppFooter.vue';
 import StudyCalendar from '@/components/calendar/StudyCalendar.vue'
 </script>
--- src/pages/GraphView.vue
+++ src/pages/GraphView.vue
@@ -1,17 +1,7 @@
 <template>
-  <div class="min-h-screen flex flex-col bg-white dark:bg-gray-950">
-    <AppHeader />
-    <main class="grow">
-      <div class="h-full">
-        <GraphView />
-      </div>
-    </main>
-    <AppFooter />
-  </div>
+  <GraphView />
 </template>
 
 <script setup lang="ts">
-  import AppHeader from '@/components/AppHeader.vue';
-  import AppFooter from '@/components/AppFooter.vue';
 import GraphView from '@/components/graphs/GraphAssembly.vue';
 </script>
